c9eb756a55fe9c1cc2e6c8f3e095bbf49bec6023
Tony Tian <t.tian4321@gmail.com>
2024-01-15 16:53:48 -0600

    Updates

---
+++ precache-manifest.44ae64215f89f3e4c468055c1e41649a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d840b20f0d3a5aca5b903ebb887a87",
+    "url": "/ToG-Landing-Page/index.html"
+  },
+  {
+    "revision": "fc958b554e8069687473",
+    "url": "/ToG-Landing-Page/static/css/main.68166fff.chunk.css"
+  },
+  {
+    "revision": "527447818c0970ab8ce9",
+    "url": "/ToG-Landing-Page/static/js/2.e7b66a9d.chunk.js"
+  },
+  {
+    "revision": "b35683604c9ad9a2bcd08581d0015451",
+    "url": "/ToG-Landing-Page/static/js/2.e7b66a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc958b554e8069687473",
+    "url": "/ToG-Landing-Page/static/js/main.4239a22a.chunk.js"
+  },
+  {
+    "revision": "7e3ac8a2e05617291adc",
+    "url": "/ToG-Landing-Page/static/js/runtime-main.0362cd0d.js"
+  }
+]);
